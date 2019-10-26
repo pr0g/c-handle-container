74876b5f72124f157f7cea69f80edc18ae1dd868
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2019-10-26 08:04:32 +0100

    small tidy-up to main.c

--- main.c
+++ main.c
@@ -5,8 +5,6 @@
 
 #include "container.h"
 
-// reminders
-// TEST_ASSERT_EQUAL_INT64(0, test_container->size_);
 // TEST_FAIL_MESSAGE("oh no");
 
 container_t* test_container = NULL;
@@ -211,25 +209,5 @@ int main(void) {
     RUN_TEST(test_addTwoHandlesAndUpdateObjects);
     RUN_TEST(test_originalHandleCannotAccessObjectAfterRemoval);
     RUN_TEST(test_objectsRemainPackedAfterRemoval);
-
     return UNITY_END();
-
-    // container_t* container = container_alloc();
-    // container_init(container);
-    // handle_t handle1 = container_add(container);
-    // handle_t handle2 = container_add(container);
-    // handle_t handle3 = container_add(container);
-    // handle_t handle4 = container_add(container);
-    // object_t* obj1 = container_get(container, handle1);
-    // obj1->value_ = 1;
-    // object_t* obj2 = container_get(container, handle2);
-    // obj2->value_ = 2;
-    // object_t* obj3 = container_get(container, handle3);
-    // obj3->value_ = 3;
-    // object_t* obj4 = container_get(container, handle4);
-    // obj4->value_ = 4;
-    // container_remove(container, handle2);
-    // object_t* obj4_again = container_get(container, handle4);
-    // container_dealloc(container);
-    // return 0;
 }
